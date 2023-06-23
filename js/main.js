45aa37e4da45bf89f46a7478d213982a43bfabba
leoleoyuuki <rm550373@fiap.com.br>
2023-06-23 15:54:00 -0300

    Start remote commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+let habilidade = document.querySelector('.skill');
+
+habilidade.addEventListener('click',()=>{
+    let sec3 = document.querySelector('.sec3');
+    sec3.scrollIntoView({behavior:'smooth'})
+});
